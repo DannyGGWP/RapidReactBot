e8243d0d6d69ff974746d4255967ae65c2dcc7c3
Big Poppa <bsimeon06142005@gmail.com>
2022-01-20 19:08:28 -0500

    Added Shooter Class

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public class Shooter extends SubsystemBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+public Shooter(){}
+  @Override 
+  public void periodic(){}
+@Override
+public void simulationPeriodic() {
+  // TODO Auto-generated method stub
+  super.simulationPeriodic();
+}
+}
